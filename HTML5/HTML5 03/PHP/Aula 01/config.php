110792e97ce91d22e9a8f5da457acc3527d7eb8a
Nathanandrademod <nat1234han@gmail.com>
2024-01-11 09:25:41 -0300

    2 COMIT

---
+++ HTML5/HTML5 03/PHP/Aula 01/config.php
@@ -0,0 +1,8 @@
+<?php  
+    $host = "localhost";
+    $port = 5432;
+    $user = "postgrs";
+    $pass = "nat12han";
+    $name = "MEUSITE";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$name";
+    ?>
